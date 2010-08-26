7d19bbcc2e72297ccbdef93803f902e026f59818
Jianjiong Gao <github@gaolab.org>
2010-08-26 16:01:38 +0000

    sequence diagram: tooltip for ptm.
    git-svn-id: file:///Users/blivens/dev/bourne/biojava_migration/svnrepo/RCSB_SequenceViewer/trunk@8216 7c6358e6-4a41-0410-a743-a5b2a554c398

--- src/main/java/org/rcsb/sequence/ptm/ModResSummary.java
+++ src/main/java/org/rcsb/sequence/ptm/ModResSummary.java
@@ -41,42 +41,8 @@ public class ModResSummary  extends AnnotationSummaryCell<ModifiedCompound> {
 
    @Override
    protected void renderAnnotation(AnnotationValue<ModifiedCompound> av, HtmlElement el) {
-//      HtmlElement colouredDomId, domDesc;
-//      DomainDefinitionValue ddv =  (DomainDefinitionValue)av;
-//      Integer numFrags;
-//      
-//      colouredDomId = new HtmlElement("span");
-//      colouredDomId.addAttribute("style", "background-color: " + ColorUtil.getArbitraryHexColor(ddv.value()));
-//      colouredDomId.addAttribute("onclick", Annotation2Jmol.getOnclick(ag, av));
-//      colouredDomId.addAttribute("class", "clickableIfJmol");
-//      colouredDomId.appendToContent("&nbsp;")
-//                   .appendToContent(av.value().toString())
-//                   .appendToContent("&nbsp;");
-//      el.addChild(colouredDomId);
-//      
-//      
-//      domDesc = new HtmlElement("span");
-//      domDesc.appendToContent("&nbsp;");
-//      if(ddv.getDescription() != null && !ddv.getDescription().equals(ddv.value()))
-//      {
-//         domDesc.appendToContent(ddv.getDescription())
-//                .appendToContent(": ");
-//      }
-//      domDesc.appendToContent( ag.getResiduesPerAnnotationValue().get(ddv).toString() )
-//             .appendToContent( " residues" );
-//      
-//      if((numFrags = ag.getAnnotationValueCount().get(ddv)) > 1)
-//      {
-//         domDesc.appendToContent(" in ")
-//                .appendToContent(numFrags.toString())
-//                .appendToContent(" fragments");
-//      }
-//      el.addChild(domDesc);
-//      
-//      if(ddv.isExternalData() && !"#".equals(ddv.getUrl()))
-//      {
-//         el.addChild(new LinkOut(ddv.getUrl(), "Link to " + ddv.getPredictionMethod() + " site"));
-//      }
-   
+	   PTMValue pv = (PTMValue)av;
+	   ModifiedCompound mc = pv.value();
+	   el.replaceContent(mc.toString());
    }
 }
