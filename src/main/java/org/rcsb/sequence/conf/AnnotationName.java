b60bed7a4c863505e7b773205f96588c74a257ff
Andreas Prlic <andreas.prlic@gmail.com>
2011-02-10 06:31:27 +0000

    PDBWW-3017  equals method is missing. As a consequence cached ViewParameters can end up not detecting the equivalence of annotations with all sorts of bad side effects.
    git-svn-id: file:///Users/blivens/dev/bourne/biojava_migration/svnrepo/RCSB_SequenceViewer/trunk@8745 7c6358e6-4a41-0410-a743-a5b2a554c398

--- src/main/java/org/rcsb/sequence/conf/AnnotationName.java
+++ src/main/java/org/rcsb/sequence/conf/AnnotationName.java
@@ -92,6 +92,33 @@ public class AnnotationName implements Serializable, Comparable<AnnotationName>{
 	}
 	
 	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		AnnotationName other = (AnnotationName) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+
+
 	public int compareTo(AnnotationName other) {
 		if ( this.equals(other))
 			return 0;
