748a5a0598a3ae3775f85131a576bae13cca20ad
Andreas Prlic <andreas.prlic@gmail.com>
2011-02-17 23:41:02 +0000

    cleanup for new constants
    git-svn-id: file:///Users/blivens/dev/bourne/biojava_migration/svnrepo/RCSB_SequenceViewer/trunk@8785 7c6358e6-4a41-0410-a743-a5b2a554c398

--- src/main/java/org/rcsb/sequence/util/AnnotationConstants.java
+++ src/main/java/org/rcsb/sequence/util/AnnotationConstants.java
@@ -9,5 +9,10 @@ public class AnnotationConstants {
 	public static final String domainParser = "Domain Parser";
 	public static final String proteinModification = "Protein Modification";
 	public static final String disulphide = "disulphide";
-	public static final String siteRecord = "SITE record";
+	public static final String siteRecord = "Site Record";
+	public static final String snp = "Single Nucleotide Polymorphism (SNP)";
+	public static final String DSSP = "DSSP";
+	public static final String STRIDE = "STRIDE";
+	public static final String CATH = "CATH";
+	public static final String PFAM = "PFAM";
 }
